5672a8223e3e5e6c32b9800dd459a249af757ba5
Rob Prouse <rob@prouse.org>
2020-04-19 14:25:41 -0400

    Add a computed value

--- 05-interacting-within-a-component/begin/vue-heroes/src/components/heroes.vue
+++ 05-interacting-within-a-component/begin/vue-heroes/src/components/heroes.vue
@@ -27,7 +27,7 @@
       <div class="column is-4" v-if="selectedHero">
         <div class="card">
           <header class="card-header">
-            <p class="card-header-title">{{ selectedHero.firstName }}</p>
+            <p class="card-header-title">{{ fullName }}</p>
           </header>
           <div class="card-content">
             <div class="content">
@@ -146,6 +146,11 @@ export default {
     },
     selectHero(hero) {
       this.selectedHero = hero;
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.selectedHero.firstName} ${this.selectedHero.lastName}`;
     },
   },
 };

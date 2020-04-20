df186f127e214b94f3ac0c101b32c019865f257a
Rob Prouse <rob@prouse.org>
2020-04-20 19:49:39 -0400

    Environment specific configurations

--- 07-accessing-data/begin/vue-heroes/src/shared/data.js
+++ 07-accessing-data/begin/vue-heroes/src/shared/data.js
@@ -1,9 +1,10 @@
 import * as axios from 'axios';
 import { format } from 'date-fns';
 import { inputDateFormat } from './constants';
+import { API } from './config';
 
 const getHeroes = async function() {
-  const response = await axios.get('api/heroes.json');
+  const response = await axios.get(`${API}/heroes.json`);
   const heroes = response.data.map(h => {
     h.originDate = format(h.originDate, inputDateFormat);
     return h;
